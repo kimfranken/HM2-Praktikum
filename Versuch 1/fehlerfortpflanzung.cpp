feddf2dba14e2c5a4af5615717d3deae6584adf8
Kim <kim@franken.mx>
2016-03-31 17:56:44 +0200

    remove "fixed"

--- Versuch 1/fehlerfortpflanzung.cpp
+++ Versuch 1/fehlerfortpflanzung.cpp
@@ -28,8 +28,6 @@ using namespace std;
 int main ()
 {
 
-  cout << "PI = " << M_PI << endl;
-
   fstream file;
   fstream file1;
   file.open("V_1.txt", ios::out);
@@ -37,15 +35,15 @@ int main ()
 
   long double V_1[41] = {M_2_PI, M_1_PI};
 
-  file << setw( 2 ) << 0 << "\t" << fixed << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_1[0]  << endl;
-  file << setw( 2 ) << 1 << "\t" << fixed << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_1[1]  << endl;
+  file << setw( 2 ) << 0 << "\t" << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_1[0]  << endl;
+  file << setw( 2 ) << 1 << "\t" << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_1[1]  << endl;
 
   file1 << setw( 2 ) << 0 << "\t" << scientific << setw( 50 ) << setprecision(45) << V_1[0]  << endl;
   file1 << setw( 2 ) << 1 << "\t" << scientific << setw( 50 ) << setprecision(45) << V_1[1]  << endl;
 
   for (int n = 2; n <= 40; n++) {
     V_1[n] = M_1_PI - V_1[n-2]*n*(n-1)/(M_PI*M_PI);
-    file << setw( 2 ) << n << "\t"  << fixed << setw( 50 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_1[n]  << endl;
+    file << setw( 2 ) << n << "\t"  << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_1[n]  << endl;
     file1 << setw( 2 ) << n << "\t"  << scientific << setw( 50 ) << setprecision(45) << V_1[n]  << endl;
 
   }
@@ -75,12 +73,12 @@ int main ()
   V_2[60] = 1./61.;
   V_2[59] = 1./60.;
 
-  file_2 << setw( 2 ) << 60 << "\t" << fixed << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_2[60]  << endl;
-  file_2 << setw( 2 ) << 59 << "\t" << fixed << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_2[59]  << endl;
+  file_2 << setw( 2 ) << 60 << "\t" << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_2[60]  << endl;
+  file_2 << setw( 2 ) << 59 << "\t" << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_2[59]  << endl;
 
   for (int n = 60; n >= 2; n--) {
     V_2[n-2] = M_PI*(1 - M_PI*V_2[n])/(n*(n-1));
-    file_2 << setw( 2 ) << n-2 << "\t"  << fixed << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_2[n-2]  << endl;
+    file_2 << setw( 2 ) << n-2 << "\t" << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_2[n-2]  << endl;
   }
 
   file_2.close();
@@ -92,7 +90,7 @@ int main ()
 
   for (int n = 0; n <= 40; n++) {
     V_3[n] = abs(V_2[n] - V_1[n]);
-    file_3 << setw( 2 ) << n << "\t"  << fixed << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_3[n]  << endl;
+    file_3 << setw( 2 ) << n << "\t"  << setw( 40 ) << setprecision(std::numeric_limits<long double>::digits10 + 1) << V_3[n]  << endl;
   }
 
   file_3.close();
