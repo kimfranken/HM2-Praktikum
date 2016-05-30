c5e62d91932c6552baa16b06af7d4b41db92198b
Kim <kim@franken.mx>
2016-05-30 20:26:33 +0200

    add alternative calculation for f(ta) and f(tb)
    spval benutzen zum Berechnen des Funktionswert zu den Zeitpunkten ta und
    tb. Vorher wurde eine Geradenfunktion verwendet. Die neuen Werte stimmen
    mit den gegeben Beispielen überein.

--- Versuch 3/flugkurve.cpp
+++ Versuch 3/flugkurve.cpp
@@ -89,6 +89,7 @@ int main(void)
 	int rc = Romberg_3(laenge, ta, tb, 1e-14, 20, L_out, fcnt, Q, E);
 
 	double polygonzug = 0;
+	double polygonzug2 = 0;
 	int i_tk = 0;
 	int i_tl = n-1;
 
@@ -96,6 +97,8 @@ int main(void)
 		if (ta <= t[i] && ta >= t[i-1] && ta != t[0]){
 			i_tk = i;
 			// Startstück dazu addieren!
+
+			// START Koor(ta) mit Geradengleichung
 			double m_x = (x[i] - x[i-1])/(t[i] - t[i-1]);
 			double m_y = (y[i] - y[i-1])/(t[i] - t[i-1]);
 			double m_z = (z[i] - z[i-1])/(t[i] - t[i-1]);
@@ -108,13 +111,29 @@ int main(void)
 			double y_ta = m_y*ta + b_y;
 			double z_ta = m_z*ta + b_z;
 
+			cout << x_ta << "/" << y_ta << "/" << z_ta << endl;
+			cout << sqrt( (x[i] - x_ta)*(x[i] - x_ta) + (y[i] - y_ta)*(y[i] - y_ta) + (z[i] - z_ta)*(z[i] - z_ta) ) << endl;
 			polygonzug += sqrt( (x[i] - x_ta)*(x[i] - x_ta) + (y[i] - y_ta)*(y[i] - y_ta) + (z[i] - z_ta)*(z[i] - z_ta) );
-			cout << polygonzug <<endl;
+			// ENDE Koor(ta) mit Geradengleichung
+
+			// START Koor(ta) mit spval
+			double ausg[3];
+			double x_ta2 = spval(n, ta, &x[0], &xb[0], &xc[0], &xd[0], &t[0], ausg);
+			double y_ta2 = spval(n, ta, &y[0], &yb[0], &yc[0], &yd[0], &t[0], ausg);
+			double z_ta2 = spval(n, ta, &z[0], &zb[0], &zc[0], &zd[0], &t[0], ausg);
+
+			cout << x_ta2 << "/" << y_ta2 << "/" << z_ta2 << endl;
+			cout << sqrt( (x[i] - x_ta2)*(x[i] - x_ta2) + (y[i] - y_ta2)*(y[i] - y_ta2) + (z[i] - z_ta2)*(z[i] - z_ta2) ) << endl;
+
+			polygonzug2 += sqrt( (x[i] - x_ta2)*(x[i] - x_ta2) + (y[i] - y_ta2)*(y[i] - y_ta2) + (z[i] - z_ta2)*(z[i] - z_ta2) );
+			// ENDE Koor(ta) mit spval
 
 		}
 		if (tb <= t[i] && tb >= t[i-1] && tb != t[n-1]){
 			i_tl = i-1;
 			// Endstück dazu addieren!
+
+			// START Koor(tb) mit Geradengleichung
 			double m_x = (x[i] - x[i-1])/(t[i] - t[i-1]);
 			double m_y = (y[i] - y[i-1])/(t[i] - t[i-1]);
 			double m_z = (z[i] - z[i-1])/(t[i] - t[i-1]);
@@ -127,18 +146,35 @@ int main(void)
 			double y_tb = m_y*tb + b_y;
 			double z_tb = m_z*tb + b_z;
 
+			cout << x_tb << "/" << y_tb << "/" << z_tb << endl;
+			cout << sqrt( (x_tb - x[i-1])*(x_tb - x[i-1]) + (y_tb - y[i-1])*(y_tb - y[i-1]) + (z_tb - z[i-1])*(z_tb - z[i-1]) ) << endl;
+
 			polygonzug += sqrt( (x_tb - x[i-1])*(x_tb - x[i-1]) + (y_tb - y[i-1])*(y_tb - y[i-1]) + (z_tb - z[i-1])*(z_tb - z[i-1]) );
-			cout << polygonzug <<endl;
+			// ENDE Koor(tb) mit Geradengleichung
+
+			// START Koor(tb) mit spval
+			double ausg[3];
+			double x_tb2 = spval(n, tb, &x[0], &xb[0], &xc[0], &xd[0], &t[0], ausg);
+			double y_tb2 = spval(n, tb, &y[0], &yb[0], &yc[0], &yd[0], &t[0], ausg);
+			double z_tb2 = spval(n, tb, &z[0], &zb[0], &zc[0], &zd[0], &t[0], ausg);
+
+			cout << x_tb2 << "/" << y_tb2 << "/" << z_tb2 << endl;
+			cout << sqrt( (x_tb2 - x[i-1])*(x_tb2 - x[i-1]) + (y_tb2 - y[i-1])*(y_tb2 - y[i-1]) + (z_tb2 - z[i-1])*(z_tb2 - z[i-1]) ) << endl;
+
+			polygonzug2 += sqrt( (x_tb2 - x[i-1])*(x_tb2 - x[i-1]) + (y_tb2 - y[i-1])*(y_tb2 - y[i-1]) + (z_tb2 - z[i-1])*(z_tb2 - z[i-1]) );
+			// ENDE Koor(tb) mit spval
 		}
 	}
 
 	for (int i = i_tk; i < i_tl; i++) {
 		polygonzug = polygonzug + sqrt( (x[i+1] - x[i])*(x[i+1] - x[i]) + (y[i+1] - y[i])*(y[i+1] - y[i]) + (z[i+1] - z[i])*(z[i+1] - z[i]) );
+		polygonzug2 = polygonzug2 + sqrt( (x[i+1] - x[i])*(x[i+1] - x[i]) + (y[i+1] - y[i])*(y[i+1] - y[i]) + (z[i+1] - z[i])*(z[i+1] - z[i]) );
 	}
 
 	cout << "Laenge der Flugstrecke in [ta,tb] = " << fixed << setprecision(11) << Q << " km" << endl;
 	cout << "...  zugehoerige Fehlerschaetzung = " << scientific << E << " km" << endl;
 	cout << setprecision(0) << fixed << "Laenge des Polygonzugs in [ta,tb] = " << setprecision(11) << polygonzug << " km" << endl;
+	cout << setprecision(0) << fixed << "Laenge des Polygonzug2 in [ta,tb] = " << setprecision(11) << polygonzug2 << " km" << endl;
 
 }
 
